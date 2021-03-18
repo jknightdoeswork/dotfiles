3f1973b24d6348cefb3c222da019422a06ac3764
Jason Knight <jason@winterpixel.com>
2021-03-18 12:16:55 -0600

    Install tweaks, no minimap, longer scrollback, no preview.

--- install_vscode_settings.sh
+++ install_vscode_settings.sh
@@ -1,3 +1,6 @@
+rm ~/.config/Code/User/keybindings.json
 ln -s -r vscode/keybindings.json ~/.config/Code/User/keybindings.json
+
+rm ~/.config/Code/User/settings.json
 ln -s -r vscode/settings.json ~/.config/Code/User/settings.json
 
